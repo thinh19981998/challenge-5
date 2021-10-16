81214a92af0809f706d615632fad968d7fb9de68
thinh19981998 <dpthinh1508@gmail.com>
2021-10-16 15:28:50 +0700

    add key for quote

--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -3,7 +3,9 @@ import Quote from './Quote';
 import './QuoteList.scss';
 
 function QuoteList({ list, author }) {
-  const quotes = list.map((item) => <Quote text={item.quoteText} />);
+  const quotes = list.map((item) => (
+    <Quote text={item.quoteText} key={item._id} />
+  ));
   return (
     <>
       <h2 className='author'>{author}</h2>
